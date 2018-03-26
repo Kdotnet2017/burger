788590f1d0b8f63f066de437390be228d64ba16a
Kei Namiri <knamiri@yahoo.com>
2018-03-26 12:04:23 -0700

    update in class

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,19 @@
 var mysql = require("mysql");
+if(process.env.NODE_ENV!=="production"){
+    require("dotenv").load();
+}
 var connection;
 if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
 }
 else {
     connection = mysql.createConnection({
-        host: "localhost",
-        user: "root",
-        password: "ChangeMe987",
-        database: "burgers_db"
+        host:process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password:process.env.DB_PASSWORD,
+        database: process.env.DB_SCHEMA
     });
 }
+
 connection.connect();
 module.exports = connection;
-
