78e95de154cfb945484b63da465791b709c3ae4f
Kei Namiri <knamiri@yahoo.com>
2018-03-25 21:51:12 -0700

    update

--- config/connection.js
+++ config/connection.js
@@ -1,18 +1,26 @@
-var mysql=require("mysql");
-var connection=mysql.createConnection({
-    host:"localhost",
-    user:"root",
-    password:"ChangeMe987",
-    database:"burgers_db"
-});
-connection.connect(function(error){
-    if(error){
+var mysql = require("mysql");
+var connection;
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "ChangeMe987",
+        database: "burgers_db"
+    });
+}
+/*
+connection.connect(function (error) {
+    if (error) {
         console.log(error);
         throw error;
     }
-    console.log("step 2 - SQL connected id: "+connection.threadId);
+    console.log("step 2 - SQL connected id: " + connection.threadId);
     console.log("--------------------------------------");
 });
-
-module.exports=connection;
+*/
+connection.connect();
+module.exports = connection;
 
